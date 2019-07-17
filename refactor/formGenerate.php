05939764c81c9cfd91011307a252c14c5bdc3c11
oploshka2 <ectb08@mail.ru>
2019-07-17 17:58:26 +0300

    merge step 1

---
+++ refactor/formGenerate.php
@@ -0,0 +1,18 @@
+<?php
+
+$form = array();
+
+// выводим форму.
+foreach ($form as $value){
+  print '<div class="group '.$value['group_class'].'">';
+  print '<div class="title">'.$value['group_name'].'</div>';
+  print '<div class="group_checkbox">';
+  foreach ($value['modules'] as $value2){
+    print '<div class="checkbox '.$value2['sistem_name'].'">';
+    print '<input type="checkbox" name="'.$value2['sistem_name'].'">';
+    print '<div class="info"><div class="help">?</div><div class="description">'.$value2['description'].'</div></div><div class="text">'.$value2['name'].'</div>';
+    print '</div>';
+  }
+  print '</div>';
+  print '</div>';
+}
